eec45a08699f97700ad98fa7dbfa03144588a8dc
janskeyz <80869239+janskeyz@users.noreply.github.com>
2023-04-29 17:19:25 +0800

    DB

--- database/src/main/java/com/example/database/MainActivity.java
+++ database/src/main/java/com/example/database/MainActivity.java
@@ -19,44 +19,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create a new instance of the database helper
+
         dbHelper = new MyDatabaseHelper(this);
 
-        // Delete all data
+
         dbHelper.deleteAllData();
 
-        // Insert data
+
         dbHelper.insertData("John", 25);
-        dbHelper.insertData("Luke", 24);
+        dbHelper.insertData("Del", 24);
 
-        // Get data
+
         getData();
 
-        // Update data
-        dbHelper.updateData(2, "Mark", 26);
 
-        // Get data
+        dbHelper.updateData(2, "Del", 26);
+
+
         getData();
 
-        // Delete Data
         dbHelper.deleteData(2);
 
-        // Get data
+
         getData();
 
 
-        // Shared Preferences
-        // Get the shared preferences object
+
         SharedPreferences prefs = getSharedPreferences("my_prefs", MODE_PRIVATE);
 
-        // Save values to shared preferences
+
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString("name", "John");
-        editor.putInt("age", 26);
+        editor.putInt("age", 25);
         editor.putBoolean("is_student", true);
         editor.commit();
 
-        // Get values from shared preferences
+
         String name = prefs.getString("name", "");
         int age = prefs.getInt("age", 0);
         boolean isStudent = prefs.getBoolean("is_student", false);
@@ -64,11 +62,11 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MainActivity", String.valueOf(age));
         Log.d("MainActivity", String.valueOf(isStudent));
 
-        // Remove a value
+
         editor.remove("name");
         editor.apply();
 
-        // Get values from shared preferences
+
         name = prefs.getString("name", "");
         age = prefs.getInt("age", 0);
         isStudent = prefs.getBoolean("is_student", false);
@@ -76,11 +74,11 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MainActivity", String.valueOf(age));
         Log.d("MainActivity", String.valueOf(isStudent));
 
-        // Clear shared preferences
+
         editor.clear();
         editor.commit();
 
-        // Get values from shared preferences
+
         name = prefs.getString("name", "");
         age = prefs.getInt("age", 0);
         isStudent = prefs.getBoolean("is_student", false);
@@ -95,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("MainActivity", "========= START =========");
         if (cursor.getCount() > 0) {
-            // Loop through the cursor to retrieve the data
+
             while (cursor.moveToNext()) {
                 int id = cursor.getInt(cursor.getColumnIndex("_id"));
                 String name = cursor.getString(cursor.getColumnIndex("name"));
@@ -113,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // Close the database helper
+
         if (dbHelper != null) {
             dbHelper.close();
         }
